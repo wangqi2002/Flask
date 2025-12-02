bfd65b07b3dd3fa6db7d10c2337c9f8a6d7399d2
大可 <79038729+wangqi2002@users.noreply.github.com>
2025-12-02 10:56:57 +0800

    path

--- app/robot/routes.py
+++ app/robot/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 
-robot_bp = Blueprint('auth', __name__)
+robot_bp = Blueprint('robot', __name__)
 
 @robot_bp.route('/')
 def index():
--- app/vision/routes.py
+++ app/vision/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-vision_bp = Blueprint('blog', __name__)
+vision_bp = Blueprint('vision', __name__)
 
 @vision_bp.route('/')
 def index():

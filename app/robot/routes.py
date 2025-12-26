2431a17087d442e7c8b0b9601611e8cdaf5c21ea
wangqi2002 <1722009706@qq.com>
2025-12-26 15:58:52 +0800

    add 路由

--- app/robot/routes.py
+++ app/robot/routes.py
@@ -2,8 +2,14 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 robot_bp = Blueprint('robot', __name__)
 
-@robot_bp.route('/')
+@robot_bp.route('/reply')
 def index():
     comment = request.values.get("question")
     print(comment)
     return "这里是机器人提供的答复"
+
+@robot_bp.route('/text')
+def index():
+    comment = request.values.get("audio")
+    print(comment)
+    return "这里是语音转文字提供的答复"

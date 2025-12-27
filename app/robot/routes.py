9fa1f99f4533c967f964731a0f3624944fd9e077
wang <wangdake12135@qq.com>
2025-12-27 14:40:34 +0800

    add audio解析接口

--- app/robot/routes.py
+++ app/robot/routes.py
@@ -2,14 +2,21 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 robot_bp = Blueprint('robot', __name__)
 
-@robot_bp.route('/reply')
+@robot_bp.route('/')
 def index():
     comment = request.values.get("question")
     print(comment)
     return "这里是机器人提供的答复"
 
-@robot_bp.route('/text')
-def index():
-    comment = request.values.get("audio")
+@robot_bp.route('/reply')
+def reply():
+    comment = request.values.get("question")
+    print(comment)
+    return "这里是机器人提供的答复"
+
+@robot_bp.route('/text' ,methods=['GET'])
+def text():
+    # comment = request.form.get('file')
+    comment = request.values.get('audio')
     print(comment)
     return "这里是语音转文字提供的答复"
